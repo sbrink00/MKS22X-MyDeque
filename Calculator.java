80185543fdbce9279ebd2e0ba7261069627bf555
sbrink00 <sbrink00@stuy.edu>
2019-03-26 10:09:12 -0400

    wrote fully functional calculator function

--- Calculator.java
+++ Calculator.java
@@ -9,11 +9,12 @@ public class Calculator{
   }
 
   public static double eval(String s){
-    String[] things = s.split(" ");
-    //System.out.println(Arrays.toString(things));
-    MyDeque<String> help = new MyDeque<String>();
+    String[] things = s.split(" ");//splits the string into an array of doubles and operations.
+    MyDeque<String> help = new MyDeque<String>();//initializes helper MyDeque.
     for (String thing: things){
-      if (!operation(thing)) help.addLast(thing);
+      if (!operation(thing)) help.addLast(thing);//adds numbers to MyDeque
+      //if the string is an operation then it removes the last
+      //two numbers and evaluates them based on what operation it is.
       else{
         Double second = Double.parseDouble(help.removeLast());
         Double first = Double.parseDouble(help.removeLast());
@@ -24,12 +25,14 @@ public class Calculator{
         else if (thing.equals("/")) val = first / second;
         else if (thing.equals("%")) val = first % second;
         String valToAdd = val + "";
-        help.addLast(valToAdd);
+        help.addLast(valToAdd);//it then adds it back into the array.
       }
     }
+    //lastly, it returns the final value.
     return Double.parseDouble(help.removeLast());
   }
 
+  //returns whether or not a given symbol is a valid operation.
   private static boolean operation(String maybeOperation){
     String[] operations = {"+", "-", "*", "/", "%"};
     for (String operation: operations) if (operation.equals(maybeOperation)) return true;
