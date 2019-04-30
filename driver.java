974e3cd8a2cf9143d17aaf3c2a492c458b719d4e
sbrink00 <sbrink00@stuy.edu>
2019-04-30 10:16:51 -0400

    trying to figure out whats wrong

--- driver.java
+++ driver.java
@@ -1,7 +1,6 @@
 public class driver{
   @SuppressWarnings("unchecked")
   public static void main(String[]args){
-    String expression = "1 2 3 4 5 + * - -";
-    System.out.println(Calculator.eval(expression));
+    MyDeque md = new MyDeque
   }
 }
