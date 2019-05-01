929a512637e556a72e6bf50e1fa44afbe5a6dd59
sbrink00 <sbrink00@stuy.edu>
2019-05-01 10:14:34 -0400

    using driver to test MyDeque

--- driver.java
+++ driver.java
@@ -1,6 +1,10 @@
 public class driver{
   @SuppressWarnings("unchecked")
   public static void main(String[]args){
-    MyDeque md = new MyDeque
+    MyDeque<Integer> md = new MyDeque<Integer>();
+    for (int i = 0; i < 100000; i++) md.addFirst(i);
+    System.out.println(md);
+    for (int i = 0; i < 100000; i++) md.removeLast();
+    System.out.println(md);
   }
 }
